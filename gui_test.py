44cbba27f4640e91ba3a76d2be4251c90b5413b4
James Gaither <gaitherj11@gmail.com>
2019-11-21 15:24:26 +0900

    gui test checkin/python version adjustment

--- gui_test.py
+++ gui_test.py
@@ -4,16 +4,44 @@ import os
 from PIL import Image, ImageTk
 import tkinter as tk
 from pathlib import Path
-
+count = 0
 all_pics = []
 image_path = Path(r"C:/Users/james.gaither/Projects/temp/photo")
 for subdir, dirs, files in os.walk(image_path):
     for file in files:
         all_pics.append(os.path.join(subdir, file))
 
-window=tk.Tk()
-window.title('Input tags')
-tag_input = tk.Entry(window)
-button = tk.Button(window, text='Submit', width=25)
-button.pack(side=tk.RIGHT)
+
+print(all_pics[0])
+def load_next_image():
+    count += 1
+    image = Image.open(all_pics[count])
+    photo = ImageTk.PhotoImage(image)
+
+    label.configure(image=photo)
+#    label = Label(window, image=photo)
+ #   label.image = photo
+  #  label.pack()
+
+image = Image.open(all_pics[count])
+photo = ImageTk.PhotoImage(image)
+label = Label(window, image=photo)
+label.image = photo
+label.pack()
+
+labelframe = LabelFrame(window)
+labelframe.pack(fill="both", expand="yes")
+left = Label(labelframe)
+
+button=Button(labelframe, padx = 5, pady = 5, text="Next",command = Click)
+button.pack(side = RIGHT)
+
+R1 = Radiobutton(labelframe, text="Choice 1", value=1)
+R1.pack(side = LEFT)
+
+R2 = Radiobutton(labelframe, text="Choice 2",  value=2)
+R2.pack(side = LEFT)
+
+
+left.pack()
 window.mainloop()
