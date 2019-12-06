fb4749e03af92ecddb3e3cd9d35a1561deb3219c
James Gaither <gaitherj11@gmail.com>
2019-12-06 11:34:06 +0900

    operational. added gui input tag option to main

--- modules/gui.py
+++ modules/gui.py
@@ -1,106 +1,116 @@
-# Tkinter testing script
-# This is semi-operational
 # Only can edit pictures in the DB already??
 
 from PIL import Image, ImageTk
 import tkinter as tk
 
 # Import Custom Modules
-from dbhandler import dbhandler
-
-# Scan DB for any non-tagged photos and return them
-db = dbhandler('C:/Users/james.gaither/Projects/temp/photo/testing_1.db')
-notag_query = db.notag_query()
-photoid_list = notag_query[0]
-untagged_photo = notag_query[1]
-
-# Set variables
-untagged_photo_count = len(untagged_photo)
-photo_number = 0
+from .dbhandler import dbhandler
 
 
-def insert_tag():
-    global photo_number
-    if tag_input.get():
-        for tag in tag_input.get().split():
-            tag_id = db.insert_tag(tag)
-            db.insert_phototag(photoid_list[photo_number], tag_id)
-    tag_input.delete(0, 'end')
-    if photo_number < (untagged_photo_count - 1):
-        change_photo('forward')
+class gui(object):
 
+    def __init__(self, db_path):
 
-def change_photo(direction):
-    global photo_number
-    if direction == 'forward':
-        photo_number += 1
-    elif direction == "back":
-        photo_number -= 1
-    else:
-        print('unknown direction')
-        return
-    imgLabel.grid_forget()
-    photo_display()
+        # Build initial DB connections
+        self.db = dbhandler(db_path)
+        self.notag_query = self.db.notag_query()
 
+        # Set variables
+        self.photoid_list = self.notag_query[0]
+        self.untagged_photo = self.notag_query[1]
+        self.untagged_photo_count = len(self.untagged_photo)
+        self.photo_number = 0
 
-def photo_display():
-    global photo_number
-    global imgLabel
+        # Build the display window
+        self.window = tk.Tk()
+        self.window.geometry("950x550")
+        self.window.title("Shadow Backup Tag Editor")
+        self.window.config(background='#303030')
+        self.back_button = tk.Button(text="<<",
+                                     command=lambda: self.change_photo('back'),
+                                     width=49, height=2)
+        self.back_button.grid(row=0, column=0, sticky='W')
+        self.input_button = tk.Button(text="Insert",
+                                      command=self.insert_tag, height=2,
+                                      background='#424242', fg='white')
+        self.input_button.grid(row=2, column=3, sticky='S')
+        self.fwd_button = tk.Button(text=">>", command=lambda:
+                                    self.change_photo('forward'),
+                                    width=49, height=2)
+        self.fwd_button.grid(row=0, column=1, sticky='E')
+        self.tag_input = tk.Text(self.window, width=25, height=2,
+                                 background='#424242')
+        self.tag_input.grid(row=2, column=2, sticky='S')
 
-    # Adjust the directional button states depending on photo location
-    if photo_number >= (untagged_photo_count - 1):
-        fwd_button.config(state='disabled')
-    else:
-        fwd_button.config(state='active')
-    if photo_number <= 0:
-        back_button.config(state='disabled')
-    else:
-        back_button.config(state='active')
+    def insert_tag(self):
+        # global photo_number
+        if self.tag_input.get('1.0', 'end'):
+            for tag in self.tag_input.get('1.0', 'end').split():
+                tag_id = self.db.insert_tag(tag)
+                self.db.insert_phototag(self.photoid_list[self.photo_number],
+                                        tag_id)
+        self.tag_input.delete('1.0', 'end')
+        if self.photo_number < (self.untagged_photo_count - 1):
+            self.change_photo('forward')
 
-    # Adjust image to a smaller size for display
-    img = Image.open(untagged_photo[photo_number])
-    image_width, image_height = img.size
-    img_ratio = image_height / image_width
-    new_img_width = 700
-    new_img_height = int(new_img_width * img_ratio)
-    img = img.resize((new_img_width, new_img_height),
-                     Image.ANTIALIAS)
-    small_img = ImageTk.PhotoImage(img)
+    def change_photo(self, direction):
+        # global photo_number
+        if direction == 'forward':
+            self.photo_number += 1
+        elif direction == "back":
+            self.photo_number -= 1
+        else:
+            print('unknown direction')
+            return
+        imgLabel.grid_forget()
+        self.photo_display()
 
-    # Build and display the image in a label
-    imgLabel = tk.Label(window, image=small_img)
-    imgLabel.image = small_img
-    imgLabel.grid(row=1, column=0, columnspan=2, rowspan=2, sticky='NWSE')
+    def photo_display(self):
+        # global self.photo_number
+        global imgLabel
 
-    # Display Photo name
-    photo_name = db.pull_name(photoid_list[photo_number])
-    photo_name_label = tk.Label(window, text=photo_name, background='#424242',
-                                anchor='w', relief="sunk", fg='white')
-    photo_name_label.grid(row=0, column=2, columnspan=2, sticky='NWSE')
+        # Adjust the directional button states depending on photo location
+        if self.photo_number >= (self.untagged_photo_count - 1):
+            self.fwd_button.config(state='disabled')
+        else:
+            self.fwd_button.config(state='active')
+        if self.photo_number <= 0:
+            self.back_button.config(state='disabled')
+        else:
+            self.back_button.config(state='active')
 
-    # Display all known tags. Not in use for now.
+        # Adjust image to a smaller size for display
+        img = Image.open(self.untagged_photo[self.photo_number])
+        image_width, image_height = img.size
+        img_ratio = image_height / image_width
+        new_img_width = 700
+        new_img_height = int(new_img_width * img_ratio)
+        img = img.resize((new_img_width, new_img_height),
+                         Image.ANTIALIAS)
+        small_img = ImageTk.PhotoImage(img)
 
-    # tag_label = tk.Label(window, text=tags, background='#424242',
-    #                     anchor='nw',borderwidth=2, relief="sunk", fg='white')
-    # tag_label.grid(row=1, column=2, columnspan=2, sticky='NWSE')
+        # Build and display the image in a label
+        imgLabel = tk.Label(self.window, image=small_img)
+        imgLabel.image = small_img
+        imgLabel.grid(row=1, column=0, columnspan=2, rowspan=2, sticky='NWSE')
 
+        # Display Photo name
+        photo_name = self.db.pull_name(self.photoid_list[self.photo_number])
+        photo_name_label = tk.Label(self.window, text=photo_name,
+                                    background='#424242', anchor='w',
+                                    relief="sunk", fg='white')
+        photo_name_label.grid(row=0, column=2, columnspan=2, sticky='NWSE')
 
-window = tk.Tk()
-window.geometry("950x550")
-window.title("Shadow Backup Tag Editor")
-window.config(background='#303030')
-back_button = tk.Button(text="<<", command=lambda: change_photo('back'),
-                        width=49, height=2)
-back_button.grid(row=0, column=0, sticky='W')
-input_button = tk.Button(text="Insert", command=insert_tag, height=2,
-                         background='#424242', fg='white')
-input_button.grid(row=2, column=3, sticky='S')
-fwd_button = tk.Button(text=">>", command=lambda: change_photo('forward'),
-                       width=49, height=2)
-fwd_button.grid(row=0, column=1, sticky='E')
-tag_input = tk.Text(window, width=25, height=2, background='#424242')
-tag_input.grid(row=2, column=2, sticky='S')
+        # Display all known tags. Not in use for now.
+
+        # tag_label = tk.Label(window, text=tags, background='#424242',
+        #                     anchor='nw',borderwidth=2, relief="sunk", 
+        #                     fg='white')
+        # tag_label.grid(row=1, column=2, columnspan=2, sticky='NWSE')
+
 
 # Start up display
-photo_display()
-window.mainloop()
+if __name__ == "__main__":
+    gui = gui()
+    gui.photo_display()
+    gui.window.mainloop()
